5eaabe1ec24819761829b794a4fc332deb756f7d
JOE-PC\Joe <jkperin@stanford.edu>
2018-01-25 13:36:21 -0800

    modified GN_model_coeff script to run simulation for 33.3 GHz spacing

--- f/GN_model_coeff_qsub.m
+++ f/GN_model_coeff_qsub.m
@@ -8,21 +8,25 @@ end
 
 Fiber = fiber(spanLength, @(l) 0.18);
 
-Df = 50e9;
+Df = 33.3e9;
+dlamb = df2dlamb(Df);
+lamb = 1525e-9:dlamb:1570e-9;
+
+% Df = 50e9;
 % dlamb = df2dlamb(Df);
 % lamb = 1522e-9:dlamb:1582e-9;
 
-lcenter = 1550e-9;
-fcenter = lambda2freq(lcenter);
-f = (-200:50:200)*1e9 + fcenter;
-lamb = freq2lambda(f);
+% lcenter = 1550e-9;
+% fcenter = lambda2freq(lcenter);
+% f = (-200:50:200)*1e9 + fcenter;
+% lamb = freq2lambda(f);
 
 %% Test GN model
 tic
 D = GN_model_coeff(lamb, Df, Fiber, l_idx);
 toc
 
-filename = sprintf('GN_model_coeff_spanLengthkm=%s_l=%d.mat', spanLengthkm, l_idx);
+filename = sprintf('GN_model_coeff_spanLengthkm=%s_Df=%dGHz_l=%d.mat', spanLengthkm, round(Df/1e9), l_idx);
 save(filename)
 
 % figure, imagesc(10*log10(D))
